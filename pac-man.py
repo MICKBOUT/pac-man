549eb1115559c312bfb6194003ba2b03d351dec8
MICKBOUT <matisse.boutte@gmail.com>
2026-07-29 14:24:03 +0200

    color and resizable cell

--- pac-man.py
+++ pac-man.py
@@ -16,26 +16,32 @@ center_x = (screen_width // 2)
 center_y = (screen_high // 2)
 running = True
 
+screen_background_color = 119, 51, 68
+maze_background_color = 11, 0, 20
+cell_wall_color = 245, 233, 226
+
 
 # -North wall: Blocks movement to the cell above. Encoded with the bit 0.
 # -East wall: Blocks movement to the cell on the right. Encoded with the bit 1.
 # -South wall: Blocks movement to the cell below. Encoded with the bit 2.
 # -West wall: Blocks movement to the cell on the left. Encoded with the bit 3.
 def draw_maze(maze_grid: list[list[int]]):
-    cell_size = 32
-
     center_x, center_y = screen_width // 2, screen_high // 2
 
-    len_maze_x, len_maze_y = len(maze_grid[0]), len(maze_grid)
-    offset_x = center_x - (len_maze_x * cell_size // 2)
-    offset_y = center_y - (len_maze_y * cell_size // 2)
+    start_x = center_x - (len_maze_x * cell_size // 2)
+    start_y = center_y - (len_maze_y * cell_size // 2)
 
+    pygame.draw.rect(
+        screen,
+        maze_background_color,
+        ((start_x, start_y), (len_maze_x * cell_size, len_maze_y * cell_size)),
+    )
     for y, line in enumerate(maze_grid):
         for x, cell in enumerate(line):
             draw_cell(
                 cell,
-                offset_x + x * cell_size,
-                offset_y + y * cell_size,
+                start_x + x * cell_size,
+                start_y + y * cell_size,
                 cell_size
             )
 
@@ -46,7 +52,7 @@ def draw_cell(wall: int, offset_x: int, offset_y: int, cell_size: int):
     if wall % 2:
         pygame.draw.line(
             screen,
-            "red",
+            cell_wall_color,
             (offset_x, offset_y),
             (offset_x + cell_size, offset_y)
         )
@@ -55,7 +61,7 @@ def draw_cell(wall: int, offset_x: int, offset_y: int, cell_size: int):
     if wall % 2:
         pygame.draw.line(
             screen,
-            "red",
+            cell_wall_color,
             (offset_x + cell_size, offset_y),
             (offset_x + cell_size, offset_y + cell_size)
         )
@@ -64,7 +70,7 @@ def draw_cell(wall: int, offset_x: int, offset_y: int, cell_size: int):
     if wall % 2:
         pygame.draw.line(
             screen,
-            "red",
+            cell_wall_color,
             (offset_x, offset_y + cell_size),
             (offset_x + cell_size, offset_y + cell_size)
         )
@@ -73,7 +79,7 @@ def draw_cell(wall: int, offset_x: int, offset_y: int, cell_size: int):
     if wall % 2:
         pygame.draw.line(
             screen,
-            "red",
+            cell_wall_color,
             (offset_x, offset_y),
             (offset_x, offset_y + cell_size)
         )
@@ -81,8 +87,15 @@ def draw_cell(wall: int, offset_x: int, offset_y: int, cell_size: int):
 
 maze_gen = mazegenerator.MazeGenerator((20, 20))
 maze_grid = maze_gen.maze
+len_maze_x, len_maze_y = len(maze_grid[0]), len(maze_grid)
+cell_size = min(
+    (screen_high - 10) // len_maze_y,
+    (screen_width - 10) // len_maze_x,
+)
+
+
 while running:
-    screen.fill((0, 0, 128))
+    screen.fill((119, 51, 68))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
