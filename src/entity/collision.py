26257db4fa08088e8218df00f34e9f309fbfd8be
Mathis Bichet <mbichet@z1r4p2.42lyon.fr>
2026-08-12 15:26:06 +0200

    fix littel bug in collision

--- src/entity/collision.py
+++ src/entity/collision.py
@@ -2,16 +2,16 @@ def collision(player, lst_ghost, cell_size):
     py, px = player.get_true_pos(cell_size)
     for ghost in lst_ghost:
         gy, gx = ghost.get_true_pos(cell_size)
-        if (gx <= px + cell_size and px + cell_size <= gx + cell_size
+        if (gx < px + cell_size and px + cell_size < gx + cell_size
            and gy == py):
             return True
-        if (gx <= px and px <= gx + cell_size
+        if (gx < px and px < gx + cell_size
            and gy == py):
             return True
-        if (gy <= py + cell_size and py + cell_size <= gy + cell_size
+        if (gy < py + cell_size and py + cell_size < gy + cell_size
            and gx == px):
             return True
-        if (gy <= py and py <= gy + cell_size
+        if (gy < py and py < gy + cell_size
            and gx == px):
             return True
     return False
