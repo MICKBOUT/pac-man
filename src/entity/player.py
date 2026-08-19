0a3af849504a82cf1139ab544c1936bccd0d43f4
Mathis Bichet <mbichet@z1r7p2.42lyon.fr>
2026-08-19 11:57:23 +0200

    add life bug and register bug

--- src/entity/player.py
+++ src/entity/player.py
@@ -12,10 +12,10 @@ class PlayerLogic(EntityLogic):
     def __init__(self,
                  maze: list[list[int]],
                  start_pos: tuple[int, int],
-                 monitor):
+                 life):
         super().__init__(maze, start_pos)
         self.buffer_direction = Direction.no_direction
-        self.life = monitor.config_data.lives
+        self.life = life
         self.dead = False
 
     @staticmethod
@@ -87,7 +87,7 @@ class PlayerDraw(PlayerLogic, EntityDraw):
     def __init__(
         self,
         maze: list[list[int]], start_pos: tuple[int, int],
-        monitor,
+        life: int,
         cell_size: int = 15
       ) -> None:
         self.images_loaded = [
@@ -95,7 +95,7 @@ class PlayerDraw(PlayerLogic, EntityDraw):
             for path in self.IMAGES_PATHS
         ]
         EntityDraw.__init__(self, cell_size)
-        PlayerLogic.__init__(self, maze, start_pos, monitor)
+        PlayerLogic.__init__(self, maze, start_pos, life)
 
     def _reszie_img(self) -> None:
         self.assets[Direction.right] = [
--- src/game.py
+++ src/game.py
@@ -41,7 +41,7 @@ class Game():
         self.player = PlayerDraw(
             self.maze.maze,
             self.maze.maze_center,
-            monitor
+            monitor.config_data.lives
         )
         self._reset_ghost(monitor)
         # to-do: change the variable size, for now it s useless...
@@ -87,7 +87,6 @@ class Game():
             ghost.update(self.player.pos)
         if collition_and_win_pacgum(self.player, self.pac_gum, monitor):
             monitor.level += 1
-            print(monitor.level)
             if monitor.level > len(monitor.config_data.level):
                 self.maze = Maze(self.maze_size,
                                  self.screen,
@@ -100,7 +99,7 @@ class Game():
             self.player = PlayerDraw(
                         self.maze.maze,
                         self.maze.maze_center,
-                        monitor
+                        self.player.life
                     )
             self._reset_ghost(monitor)
             monitor.super_pac_gum = False
@@ -111,8 +110,6 @@ class Game():
                 ghost.set_vulnerable(self.TIMER_VULNERABLE)
 
         self.frame_count += 1
-        # if self.frame_count > ...:
-        #     monitor.Menu_name = Menu_name.Win
 
     def _game_loop_draw(self, monitor: Monitor) -> None:
         screen_change = monitor.screen_change
